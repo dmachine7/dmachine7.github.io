8fbeb534090bad25508fabd54bb08fd0e00bc4c4
dmachine7 <60094449+dmachine7@users.noreply.github.com>
2021-02-07 23:44:03 +0530

    basic structure/styling done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.scss';
+import About from './Components/about/about';
+import HeroComponent from './Components/heroComponent/heroComponent';
+import Navbar from './Components/navbar/navbar';
+import Project from './Components/project/projectHome';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hellu
+    <div className='app'>
+      <BrowserRouter>
+        <Navbar />
+        <HeroComponent />
+        {/* <About /> */}
+        <Project />
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/heroComponent/heroComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './heroComponent.scss'
+
+const HeroComponent = () => {
+  return (
+    <div className='hero'>
+      <div className='hero-text'>
+        <h1>DEVANG</h1>
+        <div className='hero-underline'>
+          <span></span>
+          <span id='second-underline'></span>
+        </div>
+        <p>
+          web developer / <br />
+          web designer (occasional) / <br />
+          programmer
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default HeroComponent;
